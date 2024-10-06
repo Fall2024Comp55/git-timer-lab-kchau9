8b1f51ec3acddcc3a29211837f4e01e791755a59
Kris Chau <k_chau9@u.pacific.edu>
2024-10-05 17:16:30 -0700

    A timer object in java needs the "implements ActionListener" directive after importing javax.swing. Then, if we want to use any timer that is declared, we have to set up the event handler actionPerformed so that the program is receptive to the timer object. Lastly if we want the timer object to actually do something, we have to call .start() on it.

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,18 @@
 import acm.graphics.*;
 import acm.program.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;;
 
-public class MyFirstTimer extends GraphicsProgram {
+public class MyFirstTimer extends GraphicsProgram implements ActionListener{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
+	public static int numTimes = 0;
+	public Timer someTimerVar = new Timer(1000, this); // 1000ms = 1 second
 	private GLabel myLabel;
+	
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -15,6 +22,20 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		
+		someTimerVar.start();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		myLabel.move(5, 0);
+		numTimes+=1;
+		myLabel.setLabel("times called? " + numTimes);
+		if (numTimes >= 10)
+		{
+			someTimerVar.stop();
+		}
 	}
 	
 	public static void main(String[] args) {
