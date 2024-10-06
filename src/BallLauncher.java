237eaaaf15ea0eb85595ff84d337f04d919eb91a
Kris Chau <k_chau9@u.pacific.edu>
2024-10-05 17:28:26 -0700

    All balls move across the screen.

--- src/BallLauncher.java
+++ src/BallLauncher.java
@@ -9,6 +9,9 @@ public class BallLauncher extends GraphicsProgram{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int SIZE = 25;
+	public static final int MS = 50;
+	public static final int SPEED = 2;
+	private ArrayList<GOval> balls;
 	
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -16,11 +19,19 @@ public class BallLauncher extends GraphicsProgram{
 	}
 	
 	public void run() {
+		balls = new ArrayList<GOval>();
 		addMouseListeners();
+		Timer ballsTimer = new Timer(MS, this);
+		ballsTimer.start();
+		for (GOval ball : balls)
+		{
+			ball.move(SPEED, 0);
+		}
 	}
 	
 	public void mousePressed(MouseEvent e) {
 		GOval ball = makeBall(SIZE/2, e.getY());
+		balls.add(ball);
 		add(ball);
 	}
 	
@@ -29,6 +40,15 @@ public class BallLauncher extends GraphicsProgram{
 		temp.setColor(Color.RED);
 		temp.setFilled(true);
 		return temp;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		for (GOval ball : balls)
+		{
+			ball.move(SPEED, 0);
+		}
 	}
 	
 	public static void main(String[] args) {
